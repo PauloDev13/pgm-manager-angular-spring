aa937c001ddb369bf5f6d430b776e691fea9e666
Paulo Roberto De Morais <prmorais1302@gmail.com>
2024-04-01 15:29:19 -0300

    F - Factoring login, customer and installments pages

--- pgm-manager-angular/src/app/pages/login/login.component.ts
+++ pgm-manager-angular/src/app/pages/login/login.component.ts
@@ -1,13 +1,36 @@
 import { formatCurrency } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { NotifierService } from '../../shared/service/notifier.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export default class LoginComponent {
   protected readonly formatCurrency = formatCurrency;
+  protected readonly fb = inject(NonNullableFormBuilder);
+  protected notifierSnackbar = inject(NotifierService);
+  protected readonly route = inject(Router);
+
+  protected formLogin = this.fb.group({
+    username: ['', Validators.required],
+    password: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.formLogin.valid) {
+      console.log('FORM', this.formLogin.getRawValue());
+    }
+    this.formLogin.markAllAsTouched();
+  }
 }
